ceab88a7211ac4e1b3954e595c08e7aad5267bd5
Daniel Griffen <daniel@griffen.io>
2017-09-01 20:45:28 -0700

    make the binary do something

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,12 @@
+extern crate flif;
 extern crate structopt;
 #[macro_use]
 extern crate structopt_derive;
+
+use std::fs::File;
+use std::io::Read;
+
+use flif::error::*;
 
 use structopt::StructOpt;
 
@@ -29,8 +35,49 @@ enum Command {
 fn main() {
     let args = Args::from_args();
 
-    match args.cmd {
-        Command::Decode { .. } => {}
-        Command::Encode { .. } => eprintln!("encoding is not implemented at the moment"),
+    let result = match args.cmd {
+        Command::Decode { identify } => decode(identify, &args.input, args.output),
+        Command::Encode { .. } => encode(),
+    };
+
+    std::process::exit(match result {
+        Ok(_) => 0,
+        Err(e) => {
+            eprintln!("{}", e);
+            1
+        }
+    });
+}
+
+fn decode(identify: bool, input: &str, _output: Option<String>) -> Result<()> {
+    let file = File::open(input)?;
+
+    if identify {
+        id_file(file)
+    } else {
+        Err(Error::Unimplemented(
+            "decoding pixel data is not supported at this time",
+        ))
     }
+}
+
+fn id_file<R: Read>(reader: R) -> Result<()> {
+    let header = flif::components::header::Header::from_reader(reader)?;
+
+    if header.interlaced {
+        println!("interlaced");
+    }
+    if header.animated {
+        println!("animated, frames: {}", header.num_frames);
+    }
+    println!("channels: {:?}", header.channels);
+    println!("dimensions: {}W x {}H", header.width, header.height);
+
+    Ok(())
+}
+
+fn encode() -> Result<()> {
+    Err(Error::Unimplemented(
+        "flif.rs does not currently support encoding",
+    ))
 }
