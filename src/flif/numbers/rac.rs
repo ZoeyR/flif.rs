168c162a4a1e3d755fbec77b7c259dc38d66272d
Daniel Griffen <daniel@griffen.io>
2017-10-15 13:05:04 -0700

    add chance table updates

--- src/flif/numbers/rac.rs
+++ src/flif/numbers/rac.rs
@@ -5,7 +5,7 @@ use std::io;
 use error::*;
 use super::FlifReadExt;
 
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
 pub enum ChanceTableEntry {
     Zero,
     Sign,
@@ -15,10 +15,12 @@ pub enum ChanceTableEntry {
 
 pub struct ChanceTable {
     table: HashMap<ChanceTableEntry, u16>,
+    update_table: Vec<u16>,
 }
 
 impl ChanceTable {
-    pub fn new() -> ChanceTable {
+    pub fn new(alpha_divisor: u8, cutoff: u8) -> ChanceTable {
+        let update_table = Self::build_update_table(alpha_divisor, cutoff);
         let mut table = HashMap::new();
         table.insert(ChanceTableEntry::Zero, 1000);
         table.insert(ChanceTableEntry::Sign, 2048);
@@ -33,7 +35,28 @@ impl ChanceTable {
         table.insert(ChanceTableEntry::Mant(6), 1600);
         table.insert(ChanceTableEntry::Mant(7), 2048);
 
-        ChanceTable { table }
+        ChanceTable {
+            table,
+            update_table,
+        }
+    }
+
+    pub fn get_chance(&mut self, entry: ChanceTableEntry) -> u16 {
+        *self.table.entry(entry).or_insert(2048)
+    }
+
+    pub fn update_entry(&mut self, bit: bool, entry: ChanceTableEntry) {
+        let new_chance = {
+            let old_chance = self.table.entry(entry).or_insert(2048);
+
+            if bit {
+                self.update_table[*old_chance as usize]
+            } else {
+                4096 - self.update_table[(4096 - *old_chance) as usize]
+            }
+        };
+
+        self.table.insert(entry, new_chance);
     }
 
     fn insert_exp(table: &mut HashMap<ChanceTableEntry, u16>, sign: bool) {
@@ -47,6 +70,71 @@ impl ChanceTable {
         table.insert(ChanceTableEntry::Exp(7, sign), 2400);
         table.insert(ChanceTableEntry::Exp(8, sign), 2300);
         table.insert(ChanceTableEntry::Exp(9, sign), 2048);
+    }
+
+    fn build_update_table(alpha_divisor: u8, cutoff: u8) -> Vec<u16> {
+        let mut update_table = vec![0; 4096];
+
+        let max_chance: u16 = 4096 - cutoff as u16;
+        let mut old_chance: u16 = 0;
+
+        let mut chance_accumulator: u64 = u32::max_value() as u64;
+        for _ in 0..2048 {
+            let mut new_chance: u16 = (chance_accumulator >> 24) as u16
+                + if chance_accumulator & 0xFFFFFF > 0 {
+                    1
+                } else {
+                    0
+                };
+
+            if new_chance < old_chance {
+                new_chance = old_chance + 1;
+            }
+
+            if (old_chance != 0) && ((old_chance as usize) < update_table.len())
+                && new_chance <= max_chance as u16
+            {
+                update_table[old_chance as usize] = new_chance;
+            }
+
+            chance_accumulator +=
+                Self::update_chance_accumulator(chance_accumulator, alpha_divisor);
+            old_chance = new_chance;
+        }
+
+        // fill in the rest of the table
+        for old_chance in cutoff as u16..(max_chance + 1) {
+            const MAX: u64 = 1 + ::std::u32::MAX as u64;
+            if update_table[old_chance as usize] != 0 {
+                continue;
+            }
+
+            let mut new_chance = (old_chance as u64 * MAX + 2048) / 4096;
+            new_chance += Self::update_chance_accumulator(new_chance, alpha_divisor);
+            new_chance = (4096 * new_chance + (MAX / 2)) >> 32;
+
+            if new_chance <= old_chance as u64 {
+                new_chance = old_chance as u64 + 1;
+            }
+
+            if new_chance > max_chance as u64 {
+                new_chance = max_chance as u64;
+            }
+            update_table[old_chance as usize] = new_chance as u16;
+        }
+
+        update_table
+    }
+
+    #[inline(always)]
+    fn update_chance_accumulator(old: u64, alpha_divisor: u8) -> u64 {
+        const MAX: u64 = ::std::u32::MAX as u64;
+        let v = (MAX - old as u64 + 1) * (MAX / alpha_divisor as u64);
+        if v & 0xFFFFFFFF > 0 {
+            ((v + 1) >> 32) as u64
+        } else {
+            (v >> 32) as u64
+        }
     }
 }
 
@@ -148,9 +236,11 @@ impl<R: Read> Rac<R> {
     }
 
     pub fn read(&mut self, context: &mut ChanceTable, entry: ChanceTableEntry) -> Result<bool> {
-        let chance = context.table.entry(entry).or_insert(2048);
-        self.get(*chance as u32)
-        // TODO: update chance table
+        let chance = context.get_chance(entry);
+        let bit = self.get(chance as u32)?;
+        context.update_entry(bit, entry);
+
+        Ok(bit)
     }
 }
 
