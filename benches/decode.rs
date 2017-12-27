5c5a942f0affa8023068efa061448d0b9e2d6614
Matthias Krüger <matthias.krueger@famsik.de>
2017-12-27 02:54:27 +0100

    resources: add cutout versions of sea snail image and use them for benchmarking.

--- benches/decode.rs
+++ benches/decode.rs
@@ -6,14 +6,14 @@ extern crate test;
 use test::Bencher;
 use flif::Decoder;
 
-fn logo_full_decode() {
-    let file = std::fs::File::open("resources/flif_logo.flif").unwrap();
+fn cutout_full_decode() {
+    let file = std::fs::File::open("resources/sea_snail_cutout.flif").unwrap();
     let mut decoder = Decoder::new(file);
     let flif = decoder.decode().unwrap();
     flif.get_raw_pixels();
 }
 
 #[bench]
-fn bench_logo_full_decode(b: &mut Bencher) {
-    b.iter(|| logo_full_decode());
+fn bench_cutout_full_decode(b: &mut Bencher) {
+    b.iter(|| cutout_full_decode());
 }
