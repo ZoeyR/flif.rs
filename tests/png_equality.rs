8dffbd0af8a81885e136856cdd3bc682a846c5b0
Matthias Krüger <matthias.krueger@famsik.de>
2017-12-27 02:58:46 +0100

    tests: add sea_snail_coutout decoding test.

--- tests/png_equality.rs
+++ tests/png_equality.rs
@@ -24,6 +24,25 @@ fn sea_snail() {
 }
 
 #[test]
+fn sea_snail_cutout() {
+    let decoder = png::Decoder::new(File::open("resources/sea_snail_cutout.png").unwrap());
+    let (info, mut reader) = decoder.read_info().unwrap();
+    // Allocate the output buffer.
+    let mut buf = vec![0; info.buffer_size()];
+    // Read the next frame. Currently this function should only called once.
+    // The default options
+    reader.next_frame(&mut buf).unwrap();
+
+    let file = std::fs::File::open("resources/sea_snail_cutout.flif").unwrap();
+
+    let mut decoder = Decoder::new(file);
+    let flif = decoder.decode().unwrap();
+    let data = flif.get_raw_pixels();
+
+    assert_eq!(buf, data);
+}
+
+#[test]
 fn flif_logo() {
     let decoder = png::Decoder::new(File::open("resources/flif_logo.png").unwrap());
     let (info, mut reader) = decoder.read_info().unwrap();
