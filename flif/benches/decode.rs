db6aa030d4ee75f143d7df49b2b79b496ec68d35
Daniel Griffen <daniel@griffen.io>
2018-06-07 17:30:50 -0700

    update benchmark file location

--- flif/benches/decode.rs
+++ flif/benches/decode.rs
@@ -10,7 +10,7 @@ use std::io::{Cursor, Read};
 #[bench]
 fn bench_cutout_full_decode(b: &mut Bencher) {
     let mut data = Vec::new();
-    let mut file = std::fs::File::open("resources/sea_snail_cutout.flif").unwrap();
+    let mut file = std::fs::File::open("../resources/sea_snail_cutout.flif").unwrap();
     file.read_to_end(&mut data).unwrap();
     b.iter(|| {
         let raw = Flif::decode(Cursor::new(&data)).unwrap().get_raw_pixels();
