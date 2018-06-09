2838f09e9fec404c4fbcd41ad02b21941ea647d0
Artyom Pavlov <newpavlov@gmail.com>
2018-06-09 20:51:52 +0300

    Added fuzzy testing (#26)
    * added fuzzy testing
    
    * revert formatting change
    
    * removed artifact test
    
    * revert metadata changes
    
    * added fuzz limits

---
+++ fuzz/fuzz_targets/fuzz_flif.rs
@@ -0,0 +1,16 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate flif;
+
+use std::io::Cursor;
+
+fuzz_target!(|data: &[u8]| {
+    let limits = flif::Limits {
+        metadata_chunk: 32,
+        metadata_count: 8,
+        pixels: 1<<16,
+        maniac_nodes: 512,
+    };
+    let _ = flif::Flif::decode_with_limits(Cursor::new(data), limits)
+        .map(|img| img.get_raw_pixels());
+});
