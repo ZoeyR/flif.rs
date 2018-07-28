62698ab11d96bb5451d4b0ea2d5d6a4ebf928cc6
Daniel Griffen <daniel@griffen.io>
2018-07-28 10:24:44 -0700

    add tests

---
+++ flif/tests/channel_compact.rs
@@ -0,0 +1,27 @@
+extern crate flif;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use flif::components::Transformation;
+use flif::Decoder;
+
+#[test]
+fn invalid_tree() {
+    let file = BufReader::new(File::open("../resources/invalid_tree.flif").unwrap());
+    let image = Decoder::new(file).unwrap();
+    let info = image.info();
+
+    let expected = vec![Transformation::ChannelCompact];
+    assert_eq!(expected, info.second_header.transformations);
+}
+
+#[test]
+fn invalid_transform() {
+    let file = BufReader::new(File::open("../resources/invalid_tid.flif").unwrap());
+    let image = Decoder::new(file).unwrap();
+    let info = image.info();
+
+    let expected = vec![Transformation::ChannelCompact];
+    assert_eq!(expected, info.second_header.transformations);
+}
